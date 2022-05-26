165566b98fbc6cf95e35ef9c80d84129a03c655d
Garry Gorman <l00150690@student.lyit.ie>
2022-05-26 21:12:39 +0100

    snoring model

---
+++ snoringModel.R
@@ -0,0 +1,228 @@
+sleepData <- read.csv("sleep.csv")
+str(sleepData)
+
+
+colnames(sleepData) <- c('SnoringRate', 'RespirationRate', 
+                         'BodyTemp', 'Movement', 'BloodOxygen', 'REM', 
+                         'SleepHrs', 'HeartRate', 'StressLevel')
+
+sleepData$StressCat <- factor(sleepData$StressLevel, 
+                              labels = c("no stress", "mild stress", "moderated stress", "high stress", "extreme stress"),
+                              ordered = TRUE)
+
+# instead we control the variables
+# we will create our own new variables
+# if sex contains male insert 1 otherwise insert 0
+sleepData$noStress <- ifelse(sleepData$StressCat == "no stress", 1, 0)
+sleepData$mildStress <- ifelse(sleepData$StressCat == "mild stress", 1, 0)
+sleepData$moderateStress <- ifelse(sleepData$StressCat == "moderated stress", 1, 0)
+sleepData$highStress <- ifelse(sleepData$StressCat == "high stress", 1, 0)
+sleepData$extremeStress <- ifelse(sleepData$StressCat == "extreme stress", 1, 0)
+
+sleepData <- sleepData[c(1:8, 11:15)]
+
+# examine relationships between variables
+
+# check for linearity
+variables_of_interest <- c('SnoringRate', 'RespirationRate', 
+                           'BodyTemp', 'Movement', 'BloodOxygen', 'REM', 
+                           'SleepHrs', 'HeartRate')
+pairs(sleepData[variables_of_interest], cex.labels = 1.5)
+
+attach(sleepData)
+# scatterplots
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(3, 3)) # divide graph area in 2 columns
+# Check assumptions
+# correlation
+# there must be a correlation between sleepHrs and the other variables
+names(sleepData)
+scatter.smooth(x=SleepHrs,
+               y = SnoringRate,
+               main = "Sleep duration ~ snoring rate",
+               xlab = "Sleep duration",
+               ylab = "SnoringRate",
+               col = "blue")
+
+cor (SleepHrs, SnoringRate) # -0.92
+# looks like their are categories of snoring rate to sleep duration
+# within each category there is a positive correlation
+# however, there is a negative correlation overall
+
+scatter.smooth(x=RespirationRate,
+               y = SnoringRate,
+               main = "Snoring Rate ~ respiration rate",
+               xlab = "respiration rate (breaths per min)",
+               ylab = "Snoring Rate",
+               col = "blue")
+cor(SnoringRate, RespirationRate)
+
+scatter.smooth(x=BodyTemp,
+               y = SnoringRate,
+               main = "Snoring rate ~ body temperature",
+               xlab = "Body temp (Deg F.)",
+               ylab = "Snoring rate",
+               col = "blue")
+cor(SnoringRate, BodyTemp)
+# there seems to be an issue with those who did not sleep
+# it may be an option to remove those who did not sleep as this is a sleep study
+# if they did not sleep then perhaps they should be removed.
+
+scatter.smooth(x=Movement,
+               y = SnoringRate,
+               main = "Snoring rate ~ movement",
+               xlab = "Movement",
+               ylab = "Snoring rate", col = "blue")
+cor(SnoringRate, Movement)
+
+scatter.smooth(x=BloodOxygen,
+               y = SnoringRate,
+               main = "Snoring Rate ~ blood O2",
+               xlab = "Blood O2 level (%)",
+               ylab = "Snoring Rate",
+               col = "blue")
+cor(SnoringRate, BloodOxygen)
+
+scatter.smooth(x=REM,
+               y = SnoringRate,
+               main = "Snoring Rate ~ REM",
+               xlab = "REM",
+               ylab = "Snoring Rate",
+               col = "blue")
+cor (SnoringRate, REM)
+
+scatter.smooth(x=HeartRate,
+               y = SnoringRate,
+               main = "Snoring Rate ~ Heart Rate",
+               xlab = "Heart Rate (BPM)",
+               ylab = "Snoring Rate",
+               col = "BLUE")
+cor(SnoringRate, HeartRate)
+
+################################################################
+
+# Training and testing
+
+set.seed(1) # ensures we get the same random sample every time
+no_rows_data <- nrow(sleepData) # gets number of rows in sleepData
+sample <- sample(1:no_rows_data, size = round(0.7 * no_rows_data), replace = FALSE) # 70% of dataset
+training_data <- sleepData[sample, ] # assigns 70% of sleepData to training_data
+testing_data <- sleepData[-sample, ] # assigns the remainder to testing_data
+typeof(training_data)
+training_data <- as.data.frame(do.call(cbind, training_data))
+typeof(training_data)
+# building MLR model
+
+attach(training_data)
+fit <- lm(SnoringRate ~ HeartRate + RespirationRate + BodyTemp + Movement + 
+            BloodOxygen + REM + SleepHrs + noStress + mildStress +
+            moderateStress + highStress + extremeStress, data=training_data)
+
+# model evaluation
+summary(fit)
+# heart rate or respiration rate produce NA. This means that they are collinear and one of them adds nothing to the model
+# Therefore one of them must be removed from the model
+
+#fit <- lm(SnoringRate ~ HeartRate + BodyTemp + Movement + BloodOxygen + REM + SleepHrs + noStress + mildStress +
+ #           highStress, data=training_data)
+#summary(fit)
+
+confint(fit)
+?confint
+
+# to determine which variables are collinear
+myCors <- data.frame(cor(training_data, method = "spearman"))
+
+fit <- lm(SnoringRate ~ BodyTemp +  
+             mildStress +
+            highStress, data=training_data)
+summary(fit)
+vif(fit)
+sqrt(vif(fit)) > 2
+names(training_data)
+
+myCors <- data.frame(cor(training_data[c(1,3,7,9:12)], method = "spearman"))
+confint(fit)
+
+# normality and studentized residuals
+library(car)
+par(mfrow = c(1, 1))
+qqPlot(fit, labels=row.names(sleepData), id.method="identify", simulate=TRUE, main="Q-Q Plot")
+
+training_data["272",]
+training_data["384",]
+
+fitted(fit)["272"]
+fitted(fit)["384"]
+
+
+
+student_fit <- rstudent(fit)
+hist(student_fit,
+     breaks=10,
+     freq=FALSE,
+     xlab="Studentized Residual",
+     main="Distribution of Errors")
+rug(jitter(student_fit), col="brown")
+curve(dnorm(x, mean=mean(student_fit), sd=sd(student_fit)), add=TRUE, col="blue", lwd=2)
+lines(density(student_fit)$x, density(student_fit)$y, col="red", lwd=2, lty=2)
+legend("topright", legend = c( "Normal Curve", "Kernel Density Curve"), lty=1:2, col=c("blue","red"), cex=.7)
+
+outlierTest(fit) # identified 35 as an outlier
+# sleepData <- sleepData[-c(35),] # remove row 35
+
+
+############################################################
+
+# linearity
+
+crPlots(fit)
+
+# influencial observations
+cutoff <- 4/(nrow(training_data) - length(fit$coefficients) - 2)
+plot(fit, which = 4, cook.levels = cutoff, col = "blue")
+abline(h = cutoff, lty = 2, col = "red")
+
+avPlots(fit, ask=FALSE)
+
+influencePlot(fit, main="Influence Plot",
+              sub="Circle size is proportional to Cook's distance")
+
+################################################################
+
+# Homoscedasticity
+
+ncvTest(fit)
+
+spreadLevelPlot(fit)
+
+########################################################
+
+# global validation of linear model assumptions
+
+install.packages("gvlma")
+library(gvlma)
+gvmodel <- gvlma(fit)
+summary(gvmodel)
+
+
+###################################################
+
+# Transforming variables
+
+library(car)
+summary(powerTransform(training_data$SnoringRate)) # cant be done as many values in sleepHrs are zero
+# from spreadLevelPlot(fit) - suggested power transformation is 1.497262
+
+
+log_transform_snore <- log(training_data$SnoringRate)
+training_data$snore_log <- log_transform_snore
+
+ 
+fit_model1 <- lm(SnoringRate ~ BodyTemp +  
+                   mildStress +
+                   highStress, data=training_data)
+fit_model2 <- lm(snore_log ~ BodyTemp +  
+                   mildStress +
+                   highStress, data=training_data)
+AIC(fit_model1,fit_model2)
